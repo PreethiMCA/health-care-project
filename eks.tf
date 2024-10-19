2761bff18d115a9183a5dc8dcc39337147525707
PreethiMCA <preethi.uthayakumar@gmail.com>
2024-10-19 21:37:16 +0530

    Update eks.tf

--- eks.tf
+++ eks.tf
@@ -25,7 +25,7 @@ module "eks" {
       resolve_conflicts_on_update  = "OVERWRITE"
     }
   }
-}
+
  vpc_id                   = module.vpc.vpc_id
   subnet_ids               = module.vpc.public_subnets
   control_plane_subnet_ids = module.vpc.intra_subnets
