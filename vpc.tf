8a2c3230dbbf692ce0a50c941c23942391912d12
PreethiMCA <preethi.uthayakumar@gmail.com>
2024-10-19 20:57:54 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,17 +1,23 @@
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  version = "3.0.0"
+  version = "~> 4.0"
 
-  name = "eks-vpc"
-  cidr = "10.0.0.0/16"
+  name = local.name
+  cidr = local.vpc_cidr
 
-  azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
-  public_subnets  = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  private_subnets = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  azs             = local.azs
+  private_subnets = local.private_subnets
+  public_subnets  = local.public_subnets
+  intra_subnets   = local.intra_subnets
 
   enable_nat_gateway = true
-  single_nat_gateway = true
-  tags = {
-    "Name" = "eks-vpc"
+  map_public_ip_on_launch = true
+
+  public_subnet_tags = {
+    "kubernetes.io/role/elb" = 1
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/role/internal-elb" = 1
   }
 }
