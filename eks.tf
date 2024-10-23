7675b37ed000d50b043a92e591f3a117a559196d
PreethiMCA <preethi.uthayakumar@gmail.com>
2024-10-23 22:09:41 +0530

    Update eks.tf

--- eks.tf
+++ eks.tf
@@ -44,7 +44,7 @@ module "eks" {
       max_size     = 2
       desired_size = 2
 
-      instance_types = ["t3.micro"]
+      instance_types = ["t2.medium"]
       capacity_type  = "SPOT"
 
       tags = {
